546f0a19d3cd4240f1ed8dbd69b0b16ebfe5d778
Habab-Arif-Ahemd <hababarif23@gmail.com>
2022-06-27 15:06:34 -0700

    push

--- src/components/HotelView/Hotel-view.jsx
+++ src/components/HotelView/Hotel-view.jsx
@@ -5,59 +5,87 @@ import "../../App.css";
 import { calculateNewValue } from "@testing-library/user-event/dist/utils";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import Maps from "../Map/MapS";
-
+import './Hotel-view.css'
 export default function HotelView() {
   console.log("dddd", Hotel);
-  const render = (status) => {
-    return <h1>{status}</h1>;
-  };
-  
+
   return (
-    <div>
+    <div class="container">
+    <div class="row">
+      <div class="col">
+        1 of 2
+  
 
-<Wrapper apiKey={"AIzaSyBQynbfj6dHF89mD4nF03WAIhUmFXyo4Zk"} render={render}>
-<Maps/>
-
-</Wrapper>
-
-
-      <Row xl={1} md={6} className="g-4">
-        {Array.from(Hotel).map((a) => (
-          <div className="card">
-            <div class="card pt-5">
-              <div class="card-header">
-                <h6> {a.HotelName}</h6>
-              </div>
-              <div class="row card-body">
-                <div className="col-2">
-                  <img src={a.HotelImage[0]} class="card-img"></img>
-                </div>
-                <div className="col-4">
-                  <div className="row" class="card-title">
-                    {" "}
-                    {a.HotelName}
-                  </div>
-                  <div className="row"> {a.HotelReview}</div>
-                </div>
-
-                <div></div>
-              </div>
-            </div>
-          </div>
-          /*     <Col>
-      <Card  style={{ width: '40rem' ,margin:"1rem"}}>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col> */
-        ))}
-      </Row>
+      </div>
+      <div class="col">
+        2 of 2
+      </div>
     </div>
+    <div class="row">
+      <div class="col-2"> 
+      <div className="row " >     <Maps /> </div>
+        <div className="row side-row"> 
+        <label> Popular Filters </label> 
+
+          </div>
+        <div className="row side-row">    
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="1" id="defaultCheck1"/>
+        <label > Free airport shuttle</label>
+        </div> 
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="1" id="defaultCheck2"/>
+          <label > Free airport shuttle</label>
+          </div>
+          <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="1" id="defaultCheck3"/>
+          <label > Free airport shuttle</label>
+          </div>
+          <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="1" id="defaultCheck3"/>
+          <label > Free airport shuttle</label>
+          </div>
+         </div>
+
+
+
+         <div className="row side-row">
+ 
+
+
+         </div>
+      </div>
+      <div class="col-6">
+      <Row xl={1} md={6} className="g-4">
+            {Array.from(Hotel).map((a) => (
+              <div className="card">
+                <div class="card pt-5">
+                  <div class="card-header">
+                    <h6> {a.HotelName}</h6>
+                  </div>
+                  <div class="row card-body">
+                    <div className="col-2">
+                      <img src={a.HotelImage[0]} class="card-img"></img>
+                    </div>
+                    <div className="col-4">
+                      <div className="row" class="card-title">
+                        {" "}
+                        {a.HotelName}
+                      </div>
+                      <div className="row"> {a.HotelReview}</div>
+                    </div>
+
+                    <div></div>
+                  </div>
+                </div>
+              </div>
+       
+            ))}
+          </Row>      </div>
+      <div class="col">
+        3 of 3
+      </div>
+    </div>
+  </div>
   );
 }
--- src/components/Map/MapS.jsx
+++ src/components/Map/MapS.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
 /* AIzaSyBQynbfj6dHF89mD4nF03WAIhUmFXyo4Zk */
-export default function Maps (){
+import React, {Component} from 'react';
+import GoogleMapReact from 'google-map-react';
 
-    return(
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+export default class Maps extends Component {
+    static defaultProps = {
+        center: {
+          lat: 59.95,
+          lng: 30.33
+        },
+        zoom: 11
+      };
+    render(){
 
-        <div/>
-
-    )
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '50vh', width: '100%' }}>
+              <GoogleMapReact
+                bootstrapURLKeys={{ key:'AIzaSyBQynbfj6dHF89mD4nF03WAIhUmFXyo4Zk'}}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+              >
+                <AnyReactComponent
+                  lat={59.955413}
+                  lng={30.337844}
+                  text="My Marker"
+                />
+              </GoogleMapReact>
+            </div>
+          );
+    }
 
 }
